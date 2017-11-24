74665fe126ac1db6a42b01da7b25536951579311
alex <alex@pool-100-1-30-47.nwrknj.fios.verizon.net>
2017-11-24 23:25:38 +0800

    missing table view

--- MyFudanCampus/MyFudanCampus/SecondViewController.swift
+++ MyFudanCampus/MyFudanCampus/SecondViewController.swift
@@ -16,19 +16,34 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
     var resultArray = NSMutableArray()
     @IBOutlet weak var scoreTableView: UITableView!
     @IBOutlet weak var inputName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    @IBAction func btnSearch(_ sender: Any) {
+        resultArray = search()
         scoreTableView.reloadData()
+        /*查看输出数组的log
+        let x = 0
+        let lessonName = (resultArray[x]as! resultModel).lessonName
+        let lessonCode = (resultArray[x]as! resultModel).lessonCode
+        let creditPoint = (resultArray[x]as! resultModel).creditPoint
+        let semesterName = (resultArray[x]as! resultModel).semesterName
+        let teacherName = (resultArray[x]as! resultModel).teacherName
+        let totalStudentNumber = (resultArray[x]as! resultModel).totalStudentNumber
+        let scoreNumber = (resultArray[x]as! resultModel).scoreArray
+        print("\(lessonName) \(lessonCode) \(creditPoint) \(semesterName) \(teacherName) \(totalStudentNumber) \(scoreNumber)")
+        */
     }
+
     func search() -> NSMutableArray{
         //准备sql语句
         let mysql = "SELECT  Lesson.lessonName, Lesson.lessonCode, Lesson.creditPoint, Course.totalStudentNumber,Teacher.id AS teacherId, Teacher.name AS teacherName, Semester.name AS semesterName, Score.scoreValue, Score.studentCount FROM (Lesson INNER JOIN Course ON Lesson.id = Course.lesson_id) LEFT JOIN Score ON Course.id = Score.course_id LEFT JOIN Teacher ON Course.teacher_id = Teacher.id LEFT JOIN Semester ON Course.semester_id = Semester.id WHERE Lesson.lessonName like '%%\(inputName.text!)%%' ORDER BY Semester.name desc, Lesson.lessonCode asc, Score.scoreValue = 'A' DESC, Score.scoreValue = 'A-' DESC, Score.scoreValue = 'B+' DESC, Score.scoreValue = 'B' DESC, Score.scoreValue = 'B-' DESC, Score.scoreValue = 'C+' DESC, Score.scoreValue = 'C' DESC, Score.scoreValue = 'C-' DESC, Score.scoreValue = 'D+' DESC, Score.scoreValue = 'D' DESC, Score.scoreValue = 'D-' DESC, Score.scoreValue = 'P' DESC, Score.scoreValue = 'F' DESC"
         let dataArray = SQLiteManager.shareInstance.queryDB(sql:mysql)
         var i = 0
+        let array = NSMutableArray()
         
         while i<dataArray.count-1
         {
@@ -58,7 +73,7 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
             model.teacherName = (dataArray[test]as! scoreModel).teacherName
             model.totalStudentNumber = (dataArray[test]as! scoreModel).totalStudentNumber
             model.scoreArray = scoreArray
-            resultArray.add(model)
+            array.add(model)
             i = i+range
             //print("区间range是'\(range)'")
         }
@@ -68,22 +83,9 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let value = (dataArray[dataArray.count-1]as! scoreModel).studentCount
         var dict: [String: Int] = [String: Int]()
         dict[key] = value
-        (resultArray[resultArray.count-1]as! resultModel).scoreArray.append(dict)
-        //查看输出数组的log
-        let x = 0
-        let lessonName = (resultArray[x]as! resultModel).lessonName
-        let lessonCode = (resultArray[x]as! resultModel).lessonCode
-        let creditPoint = (resultArray[x]as! resultModel).creditPoint
-        let semesterName = (resultArray[x]as! resultModel).semesterName
-        let teacherName = (resultArray[x]as! resultModel).teacherName
-        let totalStudentNumber = (resultArray[x]as! resultModel).totalStudentNumber
-        let scoreNumber = (resultArray[x]as! resultModel).scoreArray
-        print("\(lessonName) \(lessonCode) \(creditPoint) \(semesterName) \(teacherName) \(totalStudentNumber) \(scoreNumber)")
-        return resultArray
-    }
-    
-    @IBAction func btnSearch(_ sender: Any) {
-        search()
+        (array[array.count-1]as! resultModel).scoreArray.append(dict)
+        //
+        return array
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
